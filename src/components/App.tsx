73518dc0cf0b9cf40596f98c67197e29954dd27a
Tim <tim.langner0@gmail.com>
2021-04-06 20:42:55 +0200

    :lipstick: Draw canvas

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import './App.scss';
 
-function App() {
+const App = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [context, setContext] = useState<CanvasRenderingContext2D | null>(null)
+
+  React.useEffect(() => {
+
+  }, [context]);
+
   return (
     <div className="app">
+      <canvas
+        className="canvas"
+        ref={canvasRef}
+        width={1000}
+        height={500}
+        />
     </div>
   );
 }
