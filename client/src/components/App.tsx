5c5ccec4fa2d3c85ebdb6bff6170e68f19658c83
Tim <tim.langner0@gmail.com>
2021-04-07 20:44:31 +0200

    :sparkles: Get ws connection on client

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,81 +1,28 @@
-import React, {useEffect, useState, useRef} from 'react';
-import './App.scss';
-
-type PaddlePosition = {
-  x: number;
-  y: number;
-}
+import React, {useEffect, useRef} from 'react';
+import Canvas from "./canvas/Canvas";
+import './app.scss';
 
 const App = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [context, setContext] = useState<CanvasRenderingContext2D | null>(null)
-  const [mouseYPosition, setMouseYPosition] = useState<number>(0);
-  const [canvasOffsetTop, setCanvasOffsetTop] = useState<number>(0);
-  const [paddle, setPaddle] = useState<PaddlePosition>({ x: 0, y: 0});
-
-  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
-    setMouseYPosition(event.clientY - canvasOffsetTop);
-  }
+  const ws = useRef<WebSocket | null>(null);
 
   useEffect(() => {
-    // Check if cavas reference exists
-    if (canvasRef.current) {
-      const renderCtx = canvasRef.current.getContext('2d');
+    ws.current = new WebSocket('ws://localhost:8999');
 
-      if (renderCtx) {
-        // Save canvas top-offset
-        setCanvasOffsetTop(canvasRef.current.offsetTop);
-
-        // save context to state
-        setContext(renderCtx);
-      }
+    ws.current.onopen = () => {
+      console.log('connected');
     }
 
-    // Draw start screen
-    if (context) {
-      // player 1
-      context.fillRect(5, 200, 10, 100);
-
-      // Save paddle position
-      setPaddle({ x: 5, y: 200 });
-
-      // player 2
-      context.fillRect(985, 200, 10, 100);
-
-      // ball
-      context.fillRect(485, 235, 15, 15);
+    ws.current.onmessage = (evt) => {
+      // Get coordinates for second paddle back
     }
-  }, [context]);
 
-  useEffect(() => {
-    if (context) {
-      // To prevent paddles from moving outside the canvas
-      if (mouseYPosition <= 50) setMouseYPosition(50);
-      if (mouseYPosition >= 450) setMouseYPosition(450);
-
-      // Clear paddle
-      context.clearRect(paddle.x, paddle.y, 10, 100);
-
-      // Clear path
-      context.beginPath();
-
-      // Draw new updated paddle
-      context.fillRect(5, mouseYPosition - 50, 10, 100);
-
-      // Set new paddle position
-      setPaddle({x: 5, y: mouseYPosition - 50})
+    ws.current.onclose = () => {
+      console.log('disconnected');
     }
-  }, [context, mouseYPosition, paddle.x, paddle.y]);
+  });
 
   return (
-    <div className="app" onMouseMove={(event) => handleMouseMove(event)}>
-      <canvas
-        className="canvas"
-        ref={canvasRef}
-        width={1000}
-        height={500}
-        />
-    </div>
+    <Canvas />
   );
 }
 
