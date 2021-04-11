bc5a3865d8030f0f1f370b237d990e9eb4055256
Tim <tim.langner0@gmail.com>
2021-04-11 16:34:29 +0200

    :fire: Remove rendering canvas in react

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,53 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Canvas from "./canvas/Canvas";
-import type { BallPosition } from "../types/BallPosition";
 import "./app.scss";
 
 const App = () => {
-    const [enemyPosition, setEnemyPosition] = useState<number>(200);
-    const [ballPosition, setBallPosition] = useState<BallPosition>({
-        x: 485,
-        y: 235,
-    });
-    const [ws] = useState<WebSocket>(
-        () => new WebSocket("ws://localhost:8080")
-    );
-
-    useEffect(() => {
-        ws.onopen = () => {
-            console.log("connected");
-        };
-
-        ws.onmessage = (messageEvent: MessageEvent<string>) => {
-            // Get coordinates for second paddle back
-            let message = JSON.parse(messageEvent.data);
-
-            // Check incoming websocket type
-            switch (message.type) {
-                case "ENEMY_POSITION_UPDATE":
-                    let yPosition = message.payload.y;
-                    setEnemyPosition(yPosition);
-                    break;
-                case "BALL_POSITION_UPDATE":
-                    setBallPosition(message.payload);
-                    break;
-                default:
-                    break;
-            }
-        };
-
-        ws.onclose = () => {
-            console.log("disconnected");
-        };
-    });
-
-    return (
-        <Canvas
-            ws={ws}
-            enemyPosition={enemyPosition}
-            ballPosition={ballPosition}
-        />
-    );
+    return <Canvas />;
 };
 
 export default App;
--- client/src/components/canvas/Canvas.tsx
+++ client/src/components/canvas/Canvas.tsx
@@ -1,126 +1,31 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
-import type { BallPosition } from "../../types/BallPosition";
 import "./canvas.scss";
+import { Game } from "../../Game";
 
-type WebsocketMessage<T> = {
-    type: string;
-    payload: T;
-};
-
-interface CanvasProps {
-    ws: WebSocket;
-    enemyPosition: number;
-    ballPosition: BallPosition;
-}
-
-const Canvas = ({ ws, enemyPosition, ballPosition }: CanvasProps) => {
+const Canvas = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
-    const [context, setContext] = useState<CanvasRenderingContext2D | null>(
-        null
-    );
-    const [canvasOffsetTop, setCanvasOffsetTop] = useState<number>(0);
-    const [paddleYPosition, setPaddleYPosition] = useState<number>(0);
 
     useEffect(() => {
-        if (context) {
-            // Clear paddle
-            context.clearRect(985, 0, 10, 500);
-
-            // Clear path
-            context.beginPath();
-
-            // Draw enemy paddle
-            context.fillRect(985, enemyPosition, 10, 100);
-        }
-    }, [context, enemyPosition]);
-
-    useEffect(() => {
-        if (context) {
-            // Clear field
-            context.clearRect(15, 0, 960, 500);
-
-            // Clear path
-            context.beginPath();
-
-            // Draw ball
-            context.fillRect(ballPosition.x, ballPosition.y, 15, 15);
-        }
-    }, [context, ballPosition]);
-
-    useEffect(() => {
-        // Check if cavas reference exists
         if (canvasRef.current) {
-            const renderCtx = canvasRef.current.getContext("2d");
+            const game = new Game(canvasRef.current);
 
-            if (renderCtx) {
-                // Save canvas top-offset
-                setCanvasOffsetTop(canvasRef.current.offsetTop);
-
-                // save context to state
-                setContext(renderCtx);
-            }
-        }
-
-        // Draw start screen
-        if (context) {
-            // player 1
-            context.fillRect(5, 200, 10, 100);
-
-            // Save paddle position
-            setPaddleYPosition(200);
-
-            // player 2
-            context.fillRect(985, 200, 10, 100);
-        }
-    }, [context]);
-
-    const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
-        let mouseYPosition: number = event.clientY - canvasOffsetTop;
-
-        if (context) {
-            // To prevent paddles from moving outside the canvas
-            if (mouseYPosition <= 50) mouseYPosition = 50;
-            if (mouseYPosition >= 450) mouseYPosition = 450;
-
-            // Clear paddle
-            context.clearRect(5, paddleYPosition, 10, 500);
-
-            // Clear path
-            context.beginPath();
-
-            // New updated paddle position
-            const newPosition: WebsocketMessage<{ y: number }> = {
-                type: "POSITION_UPDATE",
-                payload: {
-                    y: mouseYPosition - 50,
-                },
+            return () => {
+                game.dispose();
             };
-
-            // Send updated position to server
-            if (ws.readyState === 1) {
-                ws.send(JSON.stringify(newPosition));
-            }
-
-            // Draw new updated paddle
-            context.fillRect(5, mouseYPosition - 50, 10, 100);
-
-            // Save new paddle position
-            setPaddleYPosition(mouseYPosition - 50);
         }
-    };
+    }, []);
 
     return (
-        <div
-            className="canvas-wrapper"
-            onMouseMove={(event) => handleMouseMove(event)}
-        >
-            <canvas
-                className="canvas"
-                ref={canvasRef}
-                width={1000}
-                height={500}
-            />
+        <div className="canvas-wrapper">
+            <div className="canvas-border">
+                <canvas
+                    className="canvas"
+                    ref={canvasRef}
+                    width={1000}
+                    height={500}
+                />
+            </div>
         </div>
     );
 };
