5fdb2209da7cb5a7c1894d06348cc2ec68ad7d61
Tim <tim.langner0@gmail.com>
2021-04-16 16:59:40 +0200

    :sparkles: Add discord login auth pop-up

--- client/src/components/user-interface/account/Account.jsx
+++ client/src/components/user-interface/account/Account.jsx
@@ -6,10 +6,17 @@ import "./account.scss";
 const Account = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(true);
 
+    const handleLogin = () => {
+        window.open(
+            "https://discord.com/api/oauth2/authorize?client_id=832621678562770964&redirect_uri=https%3A%2F%2Fpong.ly%2Fauth%2Fdiscord&response_type=code&scope=email",
+            "targetWindow",
+            "width=500,height=800,location=no,top=20"
+        );
+    };
     return (
         <div className="account-container">
             <div className="login-wrapper">
-                <button className="login-button">
+                <button className="login-button" onClick={() => handleLogin()}>
                     <FontAwesomeIcon icon={faDiscord} /> Login with Discord
                 </button>
                 <p>
